9498a99266c74d42cbca795569c447c38fc693aa
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-14 20:18:58 -0300

    next-auth env vars setup

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET
+        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     }
 }
 
