6f1bfa9f783f9e401c49668cfb9ab39f0a4e0f38
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-14 20:45:32 -0300

    user model fix

--- src/models/User.ts
+++ src/models/User.ts
@@ -16,14 +16,11 @@ const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         required: true,
-        unique: true
     },
     email: {
         type: String,
-        unique: true
-    },
-    password: {
-        type: String,
+        unique: true,
+        required: true
     },
     level: {
         type: Number,
