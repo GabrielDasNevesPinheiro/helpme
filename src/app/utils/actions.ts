e345eecb9961d2ef5b8152488beb955cecd2cd5d
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-15 15:08:28 -0300

    server actions file

---
+++ src/app/utils/actions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import axios, { AxiosError } from "axios";
+
+export async function checkUser(email: string) { // check if user need to complete registration
+
+    interface UserStatus {
+        message: "NEW USER" | "REGISTERED";
+    }
+
+    try {
+        const res: UserStatus = await (await axios.get(`http://localhost:3000/api/user/${email}`)).data;
+        return res;
+    } catch(error) {
+        const parsedError = error as AxiosError;
+        console.log(parsedError.message);
+    }
+
+    
+
+  }
