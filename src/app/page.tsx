795411398f3a1ab5403fa7047dea7eb43da5d3b7
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-17 00:56:37 -0300

    Cleaned page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import { useSession, signOut } from "next-auth/react";
-
 export default function Home() {
 
-  const { data: session, status } = useSession({ required: true });
-
   return (
-    <main>
-      <span>
-        <p>Olá { session?.user?.name }, seu e-mail é { session?.user?.email }</p>
-        <button onClick={() => signOut()}>Logout</button>
-      </span>
-    </main>
+    <div>Clean</div>
   )
 }
