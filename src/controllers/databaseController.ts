ba16dc5c40a87aa591f1ace056032c22e826f229
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-17 00:54:26 -0300

    db spam prevent

--- src/controllers/databaseController.ts
+++ src/controllers/databaseController.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose';
 
-const connectDatabase = async () => mongoose.connect(process.env.MONGO_URL as string);
+const connectDatabase = async () => {
+
+    
+    if(mongoose.STATES[mongoose.connection.readyState] !== "connected")  // if already connected dont make another connection
+        mongoose.connect(process.env.MONGO_URL as string);
+};
 
 export default connectDatabase;
