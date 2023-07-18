6039759a9488d9ea58f133eabb24e3b2f6133f37
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:44:38 -0300

    changes for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import AppBar from "@/components/layout/AppBar";
 import MainLayout from "@/components/layout/MainLayout";
-import CallWidget from "@/components/widgets/callWidget/CallForm";
+import CallWidget from "@/components/widgets/callWidget/CallWidget";
 import ProfileWidget from "@/components/widgets/profileWidget/ProfileWidget";
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <MainLayout>
       <AppBar />
-      <div className="m-4">
+      <div className="m-4 space-y-4 md:flex md:space-x-4">
         <ProfileWidget />
         <CallWidget />
       </div>
