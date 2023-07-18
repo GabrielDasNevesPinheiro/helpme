ff25ad365f3d5823094d74496d35ca3fbae4ea03
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:52:24 -0300

    Issue icon add

--- src/components/widgets/callWidget/CallWidget.tsx
+++ src/components/widgets/callWidget/CallWidget.tsx
@@ -1,11 +1,12 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import CallForm from "./CallForm";
+import { BadgeAlert } from "lucide-react";
 
 export default function CallWidget() {
     return (
         <Card className="sm:max-w-md">
             <CardHeader>
-                <CardTitle>Algum problema?</CardTitle>
+                <CardTitle className="flex">Algum problema? <BadgeAlert className="ml-2"/></CardTitle>
                 <CardDescription>contate os operadores</CardDescription>
                 <CardContent>
                     <CallForm />
