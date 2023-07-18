bca3b84e12e77faa96b4e0fe0b4f7f9e19bfe9d5
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:54:04 -0300

    padding fix

--- src/components/widgets/callWidget/CallWidget.tsx
+++ src/components/widgets/callWidget/CallWidget.tsx
@@ -4,7 +4,7 @@ import { BadgeAlert } from "lucide-react";
 
 export default function CallWidget() {
     return (
-        <Card className="sm:max-w-md">
+        <Card className="sm:max-w-md p-2">
             <CardHeader>
                 <CardTitle className="flex">Algum problema? <BadgeAlert className="ml-2"/></CardTitle>
                 <CardDescription>contate os operadores</CardDescription>
