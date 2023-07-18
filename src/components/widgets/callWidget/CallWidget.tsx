30e7d30fec5f61bd296a4a27ea49535dab2bda20
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:13:41 -0300

    Form schema type definition + submit function

--- src/components/widgets/callWidget/CallWidget.tsx
+++ src/components/widgets/callWidget/CallWidget.tsx
@@ -1,6 +1,22 @@
 
+import callFormSchema from "./callFormSchema";
+import * as z from "zod";
+import { useForm } from "react-hook-form/dist/useForm";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function CallWidget() {
+
+    const form = useForm<z.infer<typeof callFormSchema>>({
+        resolver: zodResolver(callFormSchema),
+        defaultValues: {
+            description: ""
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof callFormSchema>) {
+        console.log(values);
+    }
+
     return (
         <div>Fazer chamado aqui</div>
     )
