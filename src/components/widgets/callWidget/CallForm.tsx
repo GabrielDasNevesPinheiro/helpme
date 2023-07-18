2ecd787d6e0b8d757995f87aaf349bdf0de9783c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:35:22 -0300

    CallForm to file and CallWidget creation

---
+++ src/components/widgets/callWidget/CallForm.tsx
@@ -0,0 +1,44 @@
+
+import callFormSchema from "./callFormSchema";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function CallForm() {
+
+    const form = useForm<z.infer<typeof callFormSchema>>({
+        resolver: zodResolver(callFormSchema),
+        defaultValues: {
+            description: ""
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof callFormSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField 
+                    name="description"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Descreva seu problema para os operadores</FormLabel>
+                            <FormControl>
+                                <Input placeholder="O problema é..." {...field} />
+                            </FormControl>
+                            <FormDescription>Seja direto(a) para melhor entendimento.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                )}
+                />
+                <Button type="submit">Enviar chamado</Button>
+            </form>
+        </Form>
+    )
+}
--- src/components/widgets/callWidget/CallWidget.tsx
+++ src/components/widgets/callWidget/CallWidget.tsx
@@ -1,44 +1,12 @@
-
-import callFormSchema from "./callFormSchema";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function CallWidget() {
-
-    const form = useForm<z.infer<typeof callFormSchema>>({
-        resolver: zodResolver(callFormSchema),
-        defaultValues: {
-            description: ""
-        }
-    })
-
-    function onSubmit(values: z.infer<typeof callFormSchema>) {
-        console.log(values);
-    }
-
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                <FormField 
-                    name="description"
-                    control={form.control}
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Descreva seu problema para os operadores</FormLabel>
-                            <FormControl>
-                                <Input placeholder="O problema é..." {...field} />
-                            </FormControl>
-                            <FormDescription>Seja direto(a) para melhor entendimento.</FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                )}
-                />
-                <Button type="submit">Enviar chamado</Button>
-            </form>
-        </Form>
+        <Card className="sm:max-w-md">
+            <CardHeader>
+                <CardTitle>Algum problema?</CardTitle>
+                <CardDescription>contate os operadores</CardDescription>
+            </CardHeader>
+        </Card>
     )
 }
