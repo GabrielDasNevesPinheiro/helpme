6d8875606f0e56d23864ce8606b8a986a4a3d04f
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-18 12:09:44 -0300

    Rename formSchema

--- src/components/widgets/callWidget/callFormSchema.tsx
+++ src/components/widgets/callWidget/callFormSchema.tsx
@@ -2,8 +2,10 @@
 
 import * as z from "zod";
 
-const formSchema = z.object({
+const callFormSchema = z.object({
     description: z.string()
                     .min(5, { message: "Descreva bem o problema..."})
                     .max(300, { message: "Você ultrapassou o limite de caracteres." })
 });
+
+export default callFormSchema;
