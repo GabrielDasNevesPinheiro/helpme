130a938c903ad6535cb171354203bd91b70bf7f8
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-19 15:15:48 -0300

    CallWidget skeleton layout

--- src/components/widgets/callWidget/callWidgetSkeleton.tsx
+++ src/components/widgets/callWidget/callWidgetSkeleton.tsx
@@ -1,9 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 export default function CallWidgetSkeleton() {
     return (
-        <div>
-            Loading skeleton
+        <div className="flex space-y-4 flex-col border rounded-md p-4 m-8 sm: max-w-md">
+            <Skeleton className="ml-4 w-72 h-12"/>
+            <Skeleton className="ml-4 w-52 h-4"/>
+            <Skeleton className="ml-4 w-78 h-8"/>
+            <Skeleton className="ml-4 w-52 h-4"/>
+            <Skeleton className="ml-4 w-52 h-12"/>
+
         </div>
     )
 }
