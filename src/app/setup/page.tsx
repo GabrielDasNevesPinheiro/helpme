c3a91ea7866041b604fb7901091ebfb329cf249d
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-19 18:39:22 -0300

    initial setup page code

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,8 +1,26 @@
 "use client";
 
+import AppBar from "@/components/layout/AppBar";
+import MainLayout from "@/components/layout/MainLayout";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Settings2Icon } from "lucide-react";
+
 export default function AccountSetupPage() {
     
     return (
-        <h1>Clean too</h1>
+        <MainLayout>
+            <AppBar />
+            <div className="m-auto">
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="flex">Configuração de conta <Settings2Icon className="ml-2"/></CardTitle>
+                        <CardDescription>Este procedimento é necessário.</CardDescription>
+                        <CardContent>
+                            aqui fica o form
+                        </CardContent>
+                    </CardHeader>
+                </Card>
+            </div>
+        </MainLayout>
     )
 }
