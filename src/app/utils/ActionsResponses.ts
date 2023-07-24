b79ee11580a519f24d31e21162935d20c03563e4
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-24 14:20:39 -0300

    getCalls function complete with date calc

--- src/app/utils/ActionsResponses.ts
+++ src/app/utils/ActionsResponses.ts
@@ -4,12 +4,23 @@ export type UserStatus = "NEW USER" | "REGISTERED" | "NOT REGISTERED"; // possib
 
 export type SetupResponse = "COMPANY HAS OWNER" | "NO COMPANY" | "SUCCESS" | "ERROR"; // possible return values on account setup
 
+// Parsed interface must be used as response types for front-end
 export interface ParsedUser {
     name: string,
-    email: string,
-    level: string,
-    company: string,
-    sector: string,
+    email: string
+    level: string
+    company: string
+    sector: string
+}
+
+export interface ParsedCall {
+    id: string
+    user: string
+    sector: string
+    description: string
+    status: boolean
+    time: string
+
 }
 
 export enum UserLevel {
--- src/app/utils/actions.ts
+++ src/app/utils/actions.ts
@@ -4,8 +4,8 @@ import connectDatabase from "@/controllers/databaseController";
 import { Company, ICompany } from "@/models/Company";
 import { ISector, Sector } from "@/models/Sector";
 import { IUser, User } from "@/models/User";
-import { ParsedUser, SetupResponse, UserLevel, UserStatus } from "./ActionsResponses";
-import { Call } from "@/models/Call";
+import { ParsedCall, ParsedUser, SetupResponse, UserLevel, UserStatus } from "./ActionsResponses";
+import { Call, ICall } from "@/models/Call";
 
 
 export async function checkUser(email: string): Promise<UserStatus> { // check if user need to complete registration
@@ -16,8 +16,8 @@ export async function checkUser(email: string): Promise<UserStatus> { // check i
 
         const user: IUser = (await User.findOne({ email })) as IUser;
 
-        if (!user) return "NOT REGISTERED" ;
-        if (!user?.company) return "NEW USER" ;
+        if (!user) return "NOT REGISTERED";
+        if (!user?.company) return "NEW USER";
 
     } catch (error) {
         console.log(error);
@@ -32,7 +32,7 @@ export async function getSectors(): Promise<string[]> {
     const sectorList: string[] = [];
 
     try {
-        
+
         await connectDatabase();
 
         const sectors: ISector[] = await Sector.find();
@@ -42,7 +42,7 @@ export async function getSectors(): Promise<string[]> {
         });
 
     } catch (error) {
-        console.log(error);        
+        console.log(error);
     }
 
     return sectorList;
@@ -50,7 +50,7 @@ export async function getSectors(): Promise<string[]> {
 }
 
 // this function must be used when setup a new account
-export async function setupUser({email, sector, company, level}: { email: string, sector: string, company: string, level: number}): Promise<SetupResponse> {
+export async function setupUser({ email, sector, company, level }: { email: string, sector: string, company: string, level: number }): Promise<SetupResponse> {
 
 
     company = company.toLowerCase();
@@ -62,17 +62,17 @@ export async function setupUser({email, sector, company, level}: { email: string
         const user = (await User.findOne({ email })) as IUser;
         const userSector = (await Sector.findOne({ name: sector })) as ISector;
         let userCompany = (await Company.findOne({ name: company })) as ICompany;
-        
+
         user.level = level;
         user.sector = userSector._id;
 
         // Bosses cannot be owner of another companies.
-        if(userCompany?._id && user.level == 0) return "COMPANY HAS OWNER";
+        if (userCompany?._id && user.level == 0) return "COMPANY HAS OWNER";
 
         // user levels [0 = BOSS, 1 = TI Support, 2 = Empolyee]
         if (!userCompany?._id && user.level == 0) { // if company doesnt exists and user level is BOSS level it will create a new company
-            
-            userCompany = new Company({ name: company});
+
+            userCompany = new Company({ name: company });
             userCompany.owner = user._id;
             await userCompany.save();
 
@@ -81,14 +81,14 @@ export async function setupUser({email, sector, company, level}: { email: string
 
         if (!userCompany?._id && (user.level as number) > 0) return "NO COMPANY";
 
-        if(userCompany?._id && (user.level as number) > 0) { // if company exists and user level is not BOSS level, then this user assign to company
+        if (userCompany?._id && (user.level as number) > 0) { // if company exists and user level is not BOSS level, then this user assign to company
             user.company = userCompany._id;
         }
 
         await User.updateOne({ email }, user);
 
         return "SUCCESS";
-        
+
 
     } catch (error) {
         return "ERROR";
@@ -97,7 +97,7 @@ export async function setupUser({email, sector, company, level}: { email: string
 }
 
 export async function getUserInfo(email: string): Promise<ParsedUser> {
-    
+
     const userLevels = {
         0: "Chefe",
         1: "Operador",
@@ -105,15 +105,15 @@ export async function getUserInfo(email: string): Promise<ParsedUser> {
     }
 
     try {
-        
+
         await connectDatabase();
         const user: IUser = (await User.findOne({ email })) as IUser;
-        const company: ICompany = (await Company.findOne({ _id: user.company})) as ICompany;
+        const company: ICompany = (await Company.findOne({ _id: user.company })) as ICompany;
         const sector: ISector = (await Sector.findOne({ _id: user.sector })) as ISector;
 
-        
 
-        return { 
+
+        return {
             name: user.name,
             email: user.email,
             company: company.name,
@@ -122,7 +122,7 @@ export async function getUserInfo(email: string): Promise<ParsedUser> {
         };
 
     } catch (error) {
-        return  {
+        return {
             name: "",
             email: "",
             company: "",
@@ -137,18 +137,17 @@ export async function makeCall(description: string, userInfo: ParsedUser): Promi
 
     try {
 
-
         await connectDatabase();
         const user: IUser = (await User.findOne({ email: userInfo.email })) as IUser;
         const sector: ISector = (await Sector.findOne({ name: userInfo.sector })) as ISector;
         const company: ICompany = (await Company.findOne({ name: userInfo.company.toLowerCase() })) as ICompany;
-        
-        if(user.level === UserLevel.OPERATOR) return false;
-        if(!user) return false;
-        if(!sector) return false;
-        if(!company) return false;
 
-        const call = new Call({ 
+        if (user.level === UserLevel.OPERATOR) return false;
+        if (!user) return false;
+        if (!sector) return false;
+        if (!company) return false;
+
+        const call = new Call({
             user: user._id,
             sector: sector._id,
             company: company._id,
@@ -162,8 +161,79 @@ export async function makeCall(description: string, userInfo: ParsedUser): Promi
         return true;
 
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
         return false;
     }
+}
+
+export async function getCalls(companyName: string): Promise<ParsedCall[]> {
+
+    let parsedCalls: ParsedCall[] = []
+
+    try {
+        await connectDatabase();
+
+        const company: ICompany = (await Company.findOne({ name: companyName })) as ICompany;
+        const calls: ICall[] = (await Call.find({ company })) as ICall[];
+        for (const call of calls) {
+
+            const user: IUser = (await User.findOne({ _id: call.user })) as IUser;
+            const sector: ISector = (await Sector.findOne({ _id: call.sector })) as ISector;
+
+            const timeResult = getTimeDiff(call.createdAt);
+
+            parsedCalls.push({
+                id: call._id.toString(),
+                user: user.name,
+                description: call.description,
+                sector: sector.name,
+                status: call.status,
+                time: timeResult,
+
+            })
+        }
+        console.log(parsedCalls);
+        return parsedCalls;
+
+    } catch {
+
+        return [];
+
+    }
+
+}
+
+
+
+function getTimeDiff(time: Date): string {
+
+    const now = new Date();
+    const differenceInSeconds = Math.floor((now.getTime() - time.getTime()) / 1000); // Difference in seconds
+
+    // Difference between call createdAt and date.now
+    let timeResult: string;
+    
+    if (differenceInSeconds < 60) { // if created seconds ago
+
+        timeResult = `há ${differenceInSeconds} segundo${differenceInSeconds !== 1 ? 's' : ''}`;
+
+    } else if (differenceInSeconds < 3600) { // if created minutes ago
+
+        const differenceInMinutes = Math.floor(differenceInSeconds / 60);
+        timeResult = `há ${differenceInMinutes} minuto${differenceInMinutes !== 1 ? 's' : ''}`;
+
+    } else if (differenceInSeconds < 86400) { // if created hours ago
+
+        const differenceInHours = Math.floor(differenceInSeconds / 3600);
+        timeResult = `há ${differenceInHours} hora${differenceInHours !== 1 ? 's' : ''}`;
+
+    } else {
+
+        const differenceInDays = Math.floor(differenceInSeconds / 86400); // if created days ago
+        timeResult = `há ${differenceInDays} dia${differenceInDays !== 1 ? 's' : ''}`;
+
+    }
+
+    return timeResult;
 }
