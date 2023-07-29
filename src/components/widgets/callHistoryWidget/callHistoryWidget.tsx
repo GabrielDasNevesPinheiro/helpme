d3d97e9c0b818f929cd73762990602a16d26b756
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-29 14:20:59 -0300

    CallPopover implementation

--- src/components/widgets/callHistoryWidget/callHistoryWidget.tsx
+++ src/components/widgets/callHistoryWidget/callHistoryWidget.tsx
@@ -1,13 +1,15 @@
 import { ParsedCall, ParsedUser, UserLevel } from "@/app/utils/ActionsResponses";
-import { getCalls, getUserInfo } from "@/app/utils/actions";
+import { closeCall, getCalls, getUserInfo } from "@/app/utils/actions";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
-import { BadgeAlertIcon, LucideRefreshCcw } from "lucide-react";
+import { BadgeAlertIcon, CheckCircle, HelpCircle, KanbanSquareIcon, LucideRefreshCcw, UserIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import CallHistorySkeleton from "./callHistorySkeleton";
 import { toast } from "@/components/ui/use-toast";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import CallPopover from "./callPopover";
 
 
 export default function CallHistoryWidget() {
@@ -28,52 +30,45 @@ export default function CallHistoryWidget() {
 
     function refreshCalls(toastEnabled: boolean) {
 
-        if(!(user?.level === "Funcionário"))
+        if (!(user?.level === "Funcionário"))
             getCalls(`${user?.company}`).then((res) => {
                 setCalls(res);
 
-                if(toastEnabled)
+                if (toastEnabled)
                     toast({
-                    title: "Atualização efetuada com sucesso!",
-                    description: "O Histórico de chamados está atualizado."
-                })
+                        title: "Atualização efetuada com sucesso!",
+                        description: "O Histórico de chamados está atualizado."
+                    })
             })
-            
+
     }
 
-    function showInfo(call: ParsedCall) {
-        console.log(call);
-    }
-
+    
+    
 
     if (calls.length === 0) return <CallHistorySkeleton />
 
     return (
         <ScrollArea className="border rounded-md w-auto h-80">
-            
+
             <div className="p-4">
                 <div className="flex justify-between items-center">
-                    
+
                     <span className="flex space-x-2">
                         <h3 className="text-2xl font-bold">Chamados</h3>
                         <BadgeAlertIcon />
-                    
+
                     </span>
-                    
-                    <Button variant={"ghost"} onClick={() => refreshCalls(true)}><LucideRefreshCcw/></Button>
-                
+
+                    <Button variant={"ghost"} onClick={() => refreshCalls(true)}><LucideRefreshCcw /></Button>
+
                 </div>
-                
+
                 <div className="p-4 space-y-4">
                     {calls.map((call) => (
                         <React.Fragment key={call.id}>
                             <Separator />
-                            <div className="text-sm w-auto hover:cursor-pointer hover:text-green-300" onClick={() => showInfo(call)}>
-                                <p className="text-ellipsis w-96 overflow-clip">{call.description}</p>
-                                <span className="flex space-x-2">
-                                    <p className="opacity-75">{call.time}</p>
-                                </span>
-                            </div>
+                            <CallPopover call={call}/>
                         </React.Fragment>
                     ))}
                 </div>
---
+++ src/components/widgets/callHistoryWidget/callPopover.tsx
@@ -0,0 +1,71 @@
+import { ParsedCall } from "@/app/utils/ActionsResponses";
+import { closeCall } from "@/app/utils/actions";
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { toast } from "@/components/ui/use-toast";
+import { CheckCircle, HelpCircle, KanbanSquareIcon, UserIcon } from "lucide-react";
+import { useState } from "react";
+
+interface Props {
+    call: ParsedCall
+}
+
+export default function CallPopover({ call }: Props) {
+    const [done, setDone] = useState(false);
+
+    function killCall(callID: string) {
+        closeCall(callID).then((success) => {
+            if (success) {
+                toast({
+                    title: "Chamado finalizado com êxito",
+                })
+            } else {
+                toast({
+                    title: "Erro ao finalizar chamado",
+                    description: "Tente novamente.",
+                    variant: "destructive",
+                })
+            }
+        })
+        setDone(true);
+        call.status = false;
+    }
+
+    return (
+        <Popover>
+            <PopoverTrigger>
+                <div className="text-sm w-auto hover:cursor-pointer hover:text-green-300">
+                    <p className="text-ellipsis w-96 overflow-clip text-start">{call.description}</p>
+                    <span className="flex space-x-2">
+                        <p className="opacity-75">{call.time}</p>
+                    </span>
+                </div>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="flex flex-col space-y-2 p-4">
+                    <span className="flex space-x-2">
+                        <UserIcon />
+                        <p>{call.user}</p>
+                    </span>
+                    <span className="flex space-x-2">
+                        <KanbanSquareIcon />
+                        <p>{call.sector}</p>
+                    </span>
+                    <span className="flex space-x-2">
+                        <HelpCircle />
+                        <p className={call.status ? "text-red-500" : "text-green-500"}>
+                            {call.status ? "Aberto" : "Resolvido"}
+                        </p>
+                    </span>
+                    <span className="flex space-x-2 text-sm opacity-70 self-end">
+                        <p >{call.datetime}</p>
+                    </span>
+                </div>
+                <Button className="space-x-2 w-full" onClick={() => killCall(call.id)} disabled={!call.status || done}>
+                    <CheckCircle />
+                    <p>Finalizar</p>
+                </Button>
+            </PopoverContent>
+        </Popover>
+    )
+}
