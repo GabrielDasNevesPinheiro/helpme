5ad09762aa10d4fdd38d52ae507fb299e0f96c8c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-31 15:01:22 -0300

    Color adjust and layout change

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -1,4 +1,4 @@
-import { GaugeIcon, HandIcon, Home } from "lucide-react";
+import { GaugeIcon } from "lucide-react";
 import ThemeButton from "../ui/theme-button";
 import { Button } from "../ui/button";
 
@@ -8,17 +8,13 @@ export default function AppBar() {
 
     return (
         <header className="flex p-4 text-4xl justify-between border-b">
-
             <div>
                 <Button variant={"link"} className="space-x-2">
                     <GaugeIcon />
                     <p>Dashboard</p>
                 </Button>
             </div>
-            
-
             <div className="flex space-x-4 items-center">
-
                 <ThemeButton size={28} />
             </div>
         </header>
