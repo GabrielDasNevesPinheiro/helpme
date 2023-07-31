8a1b3c2a9456d51ffda6208d92dedf7db85188c3
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-07-31 15:01:48 -0300

    Skeleton size adjust

--- src/components/widgets/callHistoryWidget/callHistorySkeleton.tsx
+++ src/components/widgets/callHistoryWidget/callHistorySkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CallHistorySkeleton() {
     return (
-        <div className="w-96 h-80 border rounded-md p-2 space-y-2 overflow-hidden">
+        <div className="w-full h-screen border rounded-md p-2 space-y-2 overflow-hidden">
             <Skeleton className="h-16 w-full"/>
             <Skeleton className="h-full w-full"/>
         </div>
