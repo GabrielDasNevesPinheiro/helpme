08b744d5d79879ea6b0e70696f50bae4dfb07e52
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-08-01 14:38:53 -0300

    nextauth_url fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
         GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        //NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         SOCKET_URL: process.env.SOCKET_URL,
     },
