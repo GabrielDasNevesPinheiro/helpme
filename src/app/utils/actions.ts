aa5aca5c3b758f929c8c488e925c2ded8a13a78c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-08-02 13:13:45 -0300

    testing bug

--- src/app/utils/actions.ts
+++ src/app/utils/actions.ts
@@ -205,8 +205,8 @@ export async function getCalls(companyName: string): Promise<ParsedCall[]> {
         
         return parsedCalls;
 
-    } catch {
-
+    } catch (err) {
+        console.log(`ERROR IN GETCALLS FUNCTION ${err}`);
         return [];
 
     }
