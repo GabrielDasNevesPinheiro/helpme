df663bae797cbbbdd7e3cb2270adeb7b9fb9d98d
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-08-14 13:24:08 -0300

    useless import

--- src/app/providers/userContextProvider.tsx
+++ src/app/providers/userContextProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { useState } from "react";
 import { getUserInfo } from "../utils/actions";
 import { ParsedUser } from "../utils/ActionsResponses";
 import { useSession } from "next-auth/react";
