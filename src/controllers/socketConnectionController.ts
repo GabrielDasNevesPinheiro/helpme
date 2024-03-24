dca268d46e03afacc48cd6fa703d13134211932d
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-24 11:25:36 -0300

    file refactoring

--- src/controllers/socketConnectionController.ts
+++
@@ -1,4 +0,0 @@
-import { ClientToServer, ServerToClient } from "@/app/utils/SocketActions";
-import { Socket, io } from "socket.io-client";
-
-export const socket: Socket<ServerToClient, ClientToServer> = io(`${process.env.SOCKET_URL}`);
