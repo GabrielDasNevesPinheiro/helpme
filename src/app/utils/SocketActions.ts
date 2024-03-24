3eaf30e90c2b39ec4b06b8dd9f80fd0852bbfce1
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-24 11:49:40 -0300

    bad files remove

--- src/app/utils/SocketActions.ts
+++
@@ -1,9 +0,0 @@
-interface ServerToClient {
-    callAlert: (callId: string) => void // socket server will use this function to send Alerts to front-end
-}
-
-interface ClientToServer {
-    sendCallAlert: (callID: string, company: string) => void // backend will use it to notify socket server
-}
-
-export type { ServerToClient, ClientToServer }
