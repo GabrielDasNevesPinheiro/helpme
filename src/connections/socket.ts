2c7141ed080c0e054b9827b304f78bd48f2a7fcd
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-24 12:22:46 -0300

    remove bad import

--- src/connections/socket.ts
+++ src/connections/socket.ts
@@ -1,4 +1,3 @@
-import { ClientToServer, ServerToClient } from "@/app/utils/SocketActions";
 import { Socket, io } from "socket.io-client";
 
 export const socket: Socket<ServerToClient, ClientToServer> = io(`${process.env.SOCKET_URL}`);
