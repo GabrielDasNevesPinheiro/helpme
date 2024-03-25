ffb1123b0dd16570c3804d7e51f69d40e2cf7d8b
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-25 18:54:35 -0300

    System theme

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -1,6 +1,7 @@
 import { GaugeIcon } from "lucide-react";
 import ThemeButton from "../ui/theme-button";
 import { Button } from "../ui/button";
+import { Label } from "../ui/label";
 
 
 export default function AppBar() {
@@ -8,14 +9,14 @@ export default function AppBar() {
 
     return (
         <header className="flex p-4 text-4xl justify-between border-b">
+            <p className="flex flex-col items-center">
+                <Label className="text-4xl"><a href="/">Helpme</a></Label>
+            </p>
             <div>
                 <Button variant={"link"} className="space-x-2">
                     <GaugeIcon />
                     <p>Dashboard</p>
                 </Button>
-            </div>
-            <div className="flex space-x-4 items-center">
-                <ThemeButton size={28} />
             </div>
         </header>
     )
