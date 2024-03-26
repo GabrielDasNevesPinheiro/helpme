6100bf5d4b7b76bb1cf05592c104ace01a099e24
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-26 11:47:27 -0300

    Provider refactored

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -2,26 +2,25 @@ import { FC } from 'react'
 import NextAuthSessionProvider from './sessionProvider'
 import { ThemeProvider } from './themeProvider'
 import UserContextProvider from './userContextProvider'
-import AuthProvider from './authProvider'
 import { Toaster } from '@/components/ui/toaster'
+import SocketProvider from './socketProvider'
 
 interface globalProviderProps {
     children: React.ReactNode
 }
 
-const GlobalProvider: FC<globalProviderProps> = ({ children }) => {
+const Providers: FC<globalProviderProps> = ({ children }) => {
     return (
         <NextAuthSessionProvider>
             <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
                 <UserContextProvider>
-                    <AuthProvider>
-                        {children}
-                        <Toaster />
-                    </AuthProvider>
+                    {children}
+                    <Toaster />
+                    <SocketProvider />
                 </UserContextProvider>
             </ThemeProvider>
         </NextAuthSessionProvider>
     )
 }
 
-export default GlobalProvider
+export default Providers
