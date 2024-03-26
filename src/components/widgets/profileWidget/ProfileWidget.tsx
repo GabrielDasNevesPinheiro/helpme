8aa7a2e9cb52d509dbbae0d51300ed0a9983b113
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-26 15:07:24 -0300

    Profile widget rework

--- src/components/widgets/profileWidget/ProfileWidget.tsx
+++ src/components/widgets/profileWidget/ProfileWidget.tsx
@@ -1,61 +1,32 @@
-"use client";
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { getUserInfo } from "@/app/actions/userActions";
 
-import { useSession } from "next-auth/react"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../ui/card";
-import { Avatar, AvatarImage } from "../../ui/avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
-import { Briefcase, BuildingIcon, InfoIcon, KanbanSquare, Mail } from "lucide-react";
-import ProfileWidgetSkeleton from "./profileWidgetSkeleton";
-import MotionDiv from "@/components/ui/animation/MotionDiv";
-import { useUserContext } from "@/app/context/UserContext";
-
-export default function ProfileWidget() {
-
-    const { data: session } = useSession();
-    const userContext = useUserContext();
-
-    if (!userContext.user) return <ProfileWidgetSkeleton />// if info is loading, display loading skeleton
-
-
+export default async function ProfileWidget() {
+    const session = await getServerSession();
+    const user = await getUserInfo(session?.user?.email!);
 
     return (
-        <MotionDiv animation="fadeIn" className="w-full h-full lg:h-auto">
-            <Card>
-                <CardHeader>
-                    <CardTitle className="flex">Informações <InfoIcon className="ml-2" /></CardTitle>
-                    <CardDescription>Lembre-se, se algo estiver errado inicie um chamado!</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <section className="flex flex-col space-y-4">
-                        <span className="flex items-center space-x-4 text-2xl">
-                            <Avatar className="border-2 border-foreground">
-                                <AvatarImage src={`${session?.user?.image}`} />
-                                <AvatarFallback>{session?.user?.name}</AvatarFallback>
-                            </Avatar>
-                            <h1>{userContext.user.name}</h1>
-                        </span>
-                        <div className="flex flex-col pl-2 space-y-2">
-                            <span className="flex space-x-2">
-                                <BuildingIcon />
-                                <p>{userContext.user.company}</p>
-                            </span>
-                            <span className="flex space-x-2">
-                                <Briefcase />
-                                <p>{userContext.user.level}</p>
-                            </span>
-                            <span className="flex space-x-2">
-                                <KanbanSquare />
-                                <p>{userContext.user.sector}</p>
-                            </span>
-                            <span className="flex space-x-2">
-                                <Mail />
-                                <p>{userContext.user.email}</p>
-                            </span>
-                        </div>
-                    </section>
-                </CardContent>
-
-            </Card>
-        </MotionDiv>
+        <div className="flex flex-col">
+            <div className="flex flex-col">
+                <div className="flex space-x-4">
+                    <figure>
+                        <Image src={`${session?.user?.image}`}
+                            loading="eager"
+                            width={200}
+                            height={200}
+                            alt="profile image"
+                            className="rounded-md border border-white/30 md:w-auto w-36"
+                        />
+                    </figure>
+                    <div className="flex flex-col space-y-2 md:text-3xl text-xl">
+                        <p>{user.name}</p>
+                        <p>{user.company}</p>
+                        <p>{user.level}</p>
+                        <p>{user.sector}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
