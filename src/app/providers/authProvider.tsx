139ba2b98d79ad8c4abb13928640cee19cb3acad
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-26 15:26:24 -0300

    auth check fix

--- src/app/providers/authProvider.tsx
+++ src/app/providers/authProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useUserContext } from "../context/UserContext";
 import { checkUser } from "../actions/userActions";
 import { useEffect } from "react";
@@ -13,7 +13,7 @@ interface Props {
 
 export default function AuthProvider({ children }: Props) { // if user has not an authorized account yet, it will redirect to registration
 
-  const { data: session } = useSession({ required: true });
+  const { data: session, status } = useSession({ required: true });
   const router = useRouter()
   const userContext = useUserContext();
 
@@ -21,6 +21,11 @@ export default function AuthProvider({ children }: Props) { // if user has not a
 
     const authCheck = async () => {
       //if username not set and not session loaded, return
+
+      if (status === "loading") return () => { }
+
+      if (!session) signIn();
+
       if (!(userContext.user.name !== "" && session?.user?.email)) return () => { };
 
       const authStatus = await checkUser(session.user.email);
--- src/components/widgets/profileWidget/ProfileWidget.tsx
+++ src/components/widgets/profileWidget/ProfileWidget.tsx
@@ -1,24 +1,20 @@
-import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { getUserInfo } from "@/app/actions/userActions";
 
 export default async function ProfileWidget() {
     const session = await getServerSession();
-    const user = await getUserInfo(session?.user?.email!);
+    const user = await getUserInfo(session?.user!.email!);
 
     return (
         <div className="flex flex-col">
             <div className="flex flex-col">
                 <div className="flex space-x-4">
-                    <figure>
-                        <Image src={`${session?.user?.image}`}
-                            loading="eager"
-                            width={200}
-                            height={200}
+                    {session?.user?.image &&
+                        <img src={session.user.image}
                             alt="profile image"
-                            className="rounded-md border border-white/30 md:w-auto w-36"
+                            className="rounded-md border border-white/30 md:w-48 w-36"
                         />
-                    </figure>
+                    }
                     <div className="flex flex-col space-y-2 md:text-3xl text-xl">
                         <p>{user.name}</p>
                         <p>{user.company}</p>
