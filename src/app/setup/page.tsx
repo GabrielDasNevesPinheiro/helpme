16b42c52965d040737be82505c4a8de095fa32ef
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-27 14:10:27 -0300

    setup initial rework

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -2,43 +2,37 @@
 
 import AppBar from "@/components/layout/AppBar";
 import MainLayout from "@/components/layout/MainLayout";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Settings2Icon } from "lucide-react";
-import AccountSetupForm from "./accountSetupForm";
+import { ArrowRightIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import AccountSetupFormSkeleton from "./accountSetupFormSkeleton";
 import { checkUser } from "../actions/userActions";
+import { Button } from "@/components/ui/button";
 
 export default function AccountSetupPage() {
 
     const { data: session } = useSession();
-    const [loading, setLoading] = useState(true);
     const router = useRouter();
 
     useEffect(() => {
-        checkUser(`${session?.user?.email}`).then((res) => {
-            setLoading(false);
+
+        const check = async () => {
+            const res = await checkUser(`${session?.user?.email}`);
             if (res === "REGISTERED") return router.push("/");
-        });
+        }
+
+        check();
     }, []);
 
     return (
         <MainLayout>
-            <AppBar />
-            <div className="m-auto">
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="flex">Configuração de conta <Settings2Icon className="ml-2" /></CardTitle>
-                        <CardDescription>Este procedimento é necessário.</CardDescription>
-                        <CardContent>
-                            {!loading ? <AccountSetupForm session={session} router={router} />
-                                : <AccountSetupFormSkeleton />
-                            }
-                        </CardContent>
-                    </CardHeader>
-                </Card>
+            <AppBar dashboard={false} />
+            <div className="w-full justify-center flex flex-col gap-y-10 items-center text-center pt-24">
+                <h1 className="text-3xl md:text-7xl">Vamos configurar sua conta 🚀</h1>
+                <div className="flex flex-col gap-y-2 w-full h-96 p-4 md:w-[700px]">
+                    <Button className="h-16 md:text-xl flex justify-between hover:bg-white/10 hover:h-20 transition-all" variant={"link"}>Registrar em organização existente<ArrowRightIcon /></Button>
+                    <Button className="h-16 md:text-xl flex justify-between hover:bg-white/10 hover:h-20 transition-all" variant={"link"}>Registrar minha organização <ArrowRightIcon /></Button>
+                </div>
             </div>
         </MainLayout>
     )
