b3f0724c436953fdb55c8900a145f866d460bf72
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-27 17:32:27 -0300

    static sectors

--- src/app/actions/sector.actions.ts
+++ src/app/actions/sector.actions.ts
@@ -1,25 +1,8 @@
 "use server";
 import connectDatabase from "@/connections/db";
-import { Sector } from "@/models/Sector";
 
 export async function getSectors(): Promise<string[]> {
 
-    const sectorList: string[] = [];
-
-    try {
-
-        await connectDatabase();
-
-        const sectors: Sector[] = await Sector.find();
-
-        sectors.map((sector) => {
-            sectorList.push(sector.name);
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
-
-    return sectorList;
+    return ["TI", "Presidência", "Logística", "RH", "Compras", "Marketing", "Operações", "Financeiro", "Produção", "SAC", "Jurídico", "Comercial"];
 
 }
