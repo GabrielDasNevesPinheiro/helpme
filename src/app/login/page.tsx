cf81d79728f9a4e95ae057baf791898154966487
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-27 11:26:59 -0300

    page title rework

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
 import LoginPage from "./LoginPage"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Helpme - SignIn',
+    description: 'Acesse sua conta e experimente o serviço',
+}
 
 export default function Login() {
     return (<LoginPage />)
