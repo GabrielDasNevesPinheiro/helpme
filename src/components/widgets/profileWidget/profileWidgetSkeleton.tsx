d33077f543b5206acf63f14688eccd6053dd3e1b
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-28 15:54:05 -0300

    rounded skeleton

--- src/components/widgets/profileWidget/profileWidgetSkeleton.tsx
+++ src/components/widgets/profileWidget/profileWidgetSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "../../ui/skeleton";
 export default function ProfileWidgetSkeleton() {
     return (
         <div className="flex gap-x-2">
-            <Skeleton className="md:w-48 md:h-48 w-36 h-36" />
+            <Skeleton className="md:w-48 md:h-48 w-36 h-36 rounded-full" />
             <div className="flex flex-col gap-y-4">
                 <Skeleton className="w-36 h-6" />
                 <Skeleton className="w-36 h-6" />
