58faf9fe9451535ed13609bb102efec42903be39
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-03-30 20:07:55 -0300

    SEO info add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,13 @@ const font = Work_Sans({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Helpme',
   description: 'Gerencie o suporte técnico em sua empresa do jeito certo. Conecte seus técnicos e funcionários na Helpme.',
+  authors: {
+    name: "Gabriel das Neves Pinheiro",
+    url: "https://www.linkedin.com/in/gabriel-pinheiro-71497722b/"
+  },
+  applicationName: "Helpme",
+  creator: "Gabriel das Neves Pinheiro",
+  keywords: ["Helpme", "IT", "Support", "Tech", "IT Support Software", "Software"],
 }
 
 export default function RootLayout({
