8167d9f0dc2153aa1a0159e2cf30d6d2e00579ca
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-04-02 19:17:37 -0300

    data add

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { getServerSession } from "next-auth"
 import { getOperatorCount, getUserCount, getUserInfo } from "../actions/user.actions";
 import DataView from "@/components/ui/dataview";
-import { getCallCount } from "../actions/call.actions";
+import { getCallCount, getCalls } from "../actions/call.actions";
+import { DataTable } from "./DataTable";
+import { columns } from "./columns";
 
 export default async function DashboardPage() {
     const session = await getServerSession();
@@ -11,7 +13,10 @@ export default async function DashboardPage() {
     const { company } = await getUserInfo(email);
     const userCount = await getUserCount(email);
     const operatorCount = await getOperatorCount(email);
-    const callCount = await getCallCount(email);
+    const callData = await getCalls(email);
+    const callCount = callData.length;
+    const pendingCallCount = callData.filter((call) => call.status).length;
+    const solvedCallCount = callData.filter((call) => !call.status).length;
 
     /**
      * Chamados criados pelo usuário
@@ -21,14 +26,19 @@ export default async function DashboardPage() {
      */
 
     return (
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col ">
             <div className="p-4 text-3xl sm:text-4xl md:text-5xl font-bold">
                 <p>{company}</p>
             </div>
-            <div className="flex gap-4 p-2 flex-col md:flex-row">
+            <div className="flex gap-4 p-2 flex-col md:flex-row flex-wrap">
                 <DataView description="Total de cadastros" data={userCount} />
                 <DataView description="Operadores" data={operatorCount} />
                 <DataView description="Total de chamados" data={callCount} />
+                <DataView description="Chamados abertos" data={pendingCallCount} />
+                <DataView description="Chamados resolvidos" data={solvedCallCount} />
+            </div>
+            <div className="p-2 border  m-2 rounded-md">
+                <DataTable columns={columns} data={callData} />
             </div>
         </div>
     )
