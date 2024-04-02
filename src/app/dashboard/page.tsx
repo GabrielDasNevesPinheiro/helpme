f1bec32a4dba8363376e907e60fdcf0ad540482d
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-04-02 14:37:36 -0300

    dataview components add

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,35 @@
+import { getServerSession } from "next-auth"
+import { getOperatorCount, getUserCount, getUserInfo } from "../actions/user.actions";
+import DataView from "@/components/ui/dataview";
+import { getCallCount } from "../actions/call.actions";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await getServerSession();
+
+    const email = String(session?.user?.email);
+
+    const { company } = await getUserInfo(email);
+    const userCount = await getUserCount(email);
+    const operatorCount = await getOperatorCount(email);
+    const callCount = await getCallCount(email);
+
+    /**
+     * Chamados criados pelo usuário
+     * Chamados atendidos
+     * Quem mais resolveu seus problemas
+     * 
+     */
 
     return (
-        <div>Overview here</div>
+        <div className="w-full flex flex-col">
+            <div className="p-4 text-3xl sm:text-4xl md:text-5xl font-bold">
+                <p>{company}</p>
+            </div>
+            <div className="flex gap-4 p-2 flex-col md:flex-row">
+                <DataView description="Total de cadastros" data={userCount} />
+                <DataView description="Operadores" data={operatorCount} />
+                <DataView description="Total de chamados" data={callCount} />
+            </div>
+        </div>
     )
 }
