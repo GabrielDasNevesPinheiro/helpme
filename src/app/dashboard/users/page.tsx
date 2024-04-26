bd351af96edf30abd42c69b8cdba6d97695bce93
gabriel <gabrieldasnevespinheiro@gmail.com>
2024-04-26 11:41:41 -0300

    Header text

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,5 +1,13 @@
-export default function UsersPage() {
+import UsersTable from "@/components/layout/UsersTable";
+
+export default async function UsersPage() {
+
     return (
-        <div>Users page here</div>
+        <div className="flex flex-col p-4">
+            <div className="p-4">
+                <h1 className="md:text-4xl text-2xl font-bold">Registrados na organização</h1>
+            </div>
+            <UsersTable />
+        </div>
     )
 }
